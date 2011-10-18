dcd25fb0769e65182118cd623481a98661f55368
Pedro Belo <pedro@heroku.com>
2011-10-18 15:38:40 -0700

    support canvas

--- app.rb
+++ app.rb
@@ -75,6 +75,11 @@ get "/" do
   erb :index
 end
 
+# used by Canvas apps - redirect the POST to be a regular GET
+post "/" do
+  redirect "/"
+end
+
 # used to close the browser window opened to post to wall/send to friends
 get "/close" do
   "<body onload='window.close();'/>"
